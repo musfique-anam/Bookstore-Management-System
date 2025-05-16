fc79e9f837032113d75efb0b5c8b1a08a7c6bc33
Musfique Anam <musfique2002.personal@gmail.com>
2025-05-16 09:29:58 +0600

    working

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function goToPage1() {
 function goToPage0() {
     window.location.href = "index.html"; 
 }
+
+function goToPage2() {
+    window.location.href = "index3.html"; 
+}
+
+function goToPage3() {
+    window.location.href = "index4.html"; 
+}
+
+function deleteItem() {
+    const confirmDelete = confirm("Are you sure you want to delete this book?");
+        if (confirmDelete) {
+            alert("Book deleted!");
+        }
+}
+
+//  function editBook() {
+//       alert("Edit book logic here.");
+//     }
